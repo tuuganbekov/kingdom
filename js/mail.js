a441c43d43ed7d8056ff440d3af7118121d60da8
tuuganbekov <tuuganbekovb@gmail.com>
2019-10-09 15:22:25 +0600

    fixed bugs in form handling

--- js/mail.js
+++ js/mail.js
@@ -1,23 +1,24 @@
-document.getElementById("contact-form").addEventListener("submit", sendEmail(event))
-
-function sendEmail(event) {
+document.getElementById("contact-form").addEventListener("submit", function(event){
     event.preventDefault()
+    sendEmail()
+})
+
+function sendEmail() {
     Email.send({
-        SecureToken : "52ce27ab-20df-4a87-a372-e6eb04b6a850",
-        To : 'kingdomdev.io@gmail.com',
-        From : 'kingdomdev.io@gmail.com',
+        SecureToken: "1778863d-b8ee-4e57-a584-700d1deffc67",
+        To : 'btuuganbekov@gmail.com',
+        From : "kingdomdev.io@gmail.com",
         Subject : "Заявка с сайта kingdom.io",
-        Body : `From:  ${document.getElementById('name').value}
-                Email: ${document.getElementById('email').value}
-                Phone: ${document.getElementById('phone').value}
-                Message: ${document.getElementById('message').value}`
+        Body : `From:  ${document.getElementById('name').value} \n
+        Email: ${document.getElementById('email').value}
+        Phone: ${document.getElementById('phone').value}
+        Message: ${document.getElementById('message').value}`
     }).then(
-        message => message === 'OK' ? alert("Сообщение отравлено") : alert(message)
-    ).then(() => {
+      message => message === "OK" ? alert("Сообщение отправлено") : alert(message)
+    ).then( () => {
         document.getElementById('name').value = ""
         document.getElementById('email').value = ""
         document.getElementById('phone').value = ""
         document.getElementById('message').value = ""
-    }
-    )
+    })
 }
