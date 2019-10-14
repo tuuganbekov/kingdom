1272d93a65252ff2d652ec00f6c7c01c55d02a3d
tuuganbekov <tuuganbekovb@gmail.com>
2019-10-14 10:27:58 +0600

    form handling bug fixed

--- js/mail.js
+++ js/mail.js
@@ -1,3 +1,5 @@
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } }
+
 document.getElementById("contact-form").addEventListener("submit", function(event){
     event.preventDefault()
     sendEmail()
@@ -5,11 +7,11 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
 function sendEmail() {
     Email.send({
-        SecureToken: "1778863d-b8ee-4e57-a584-700d1deffc67",
+        SecureToken: "ae47d805-36be-419d-bc32-dc5429a86765",
         To : 'btuuganbekov@gmail.com',
         From : "kingdomdev.io@gmail.com",
         Subject : "Заявка с сайта kingdom.io",
-        Body : `From:  ${document.getElementById('name').value} \n
+        Body : `From:  ${document.getElementById('name').value} 
         Email: ${document.getElementById('email').value}
         Phone: ${document.getElementById('phone').value}
         Message: ${document.getElementById('message').value}`
